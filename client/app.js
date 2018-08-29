d7718baf5ab9989a9ce04118a8bb674d38602ada
Christy Welsh <christyannwelsh@gmail.com>
2018-08-29 10:08:21 -0700

    add post request

--- client/app.js
+++ client/app.js
@@ -1,34 +1,36 @@
 $(document).ready(function () {
 
-
-    // create jquery element to display all chirps
-    //let allChirps = document.createElement('div')
-
     let chirps = [];
+    let user;
+    let text;
 
     // handle API request
     function handleResponse(data) {
         let entries = Object.entries(data)
-
-        console.log(entries)
-
         for (const [id, chirp] of entries) {
-            chirps.push(`${id} ${chirp.user}: ${chirp.text}`);
-            
+            chirps.push(`${id} ${chirp.user}: ${chirp.text}`);           
         }
         chirps.pop();
         chirps.map(chirp => $('<p></p>').text(chirp).attr('id', 'chirps').appendTo('.current'))          // create a div containing all the chirps, append to body
     }
 
+    // use get request to call api
+    $.get('http://127.0.0.1:3000/api/chirps').then(handleResponse).catch(err => console.log(err));  // or use localhost:3000
 
-
-
-    // use ajax request to call api
-    $.ajax({
-        method: "GET",
-        url: 'http://127.0.0.1:3000/api/chirps',        // or use localhost:3000
-    }).then(handleResponse)
+    // on button click, get the value of user input and ...
+    $('button').click(() => {
+        user = $('#user').val();
+        text = $('#text').val();
+        // make a post request with those values
+        $.ajax({
+            type: "POST",
+            url: 'http://127.0.0.1:3000/api/chirps/',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({"user": `${user}`, "text": `${text}`})
+        })
+        .then(console.log('posted'))
         .catch(err => console.log(err));
+    })
 
 
 
