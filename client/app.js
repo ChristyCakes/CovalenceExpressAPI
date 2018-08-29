431152e8ff1c3e3a21203160e8682ea4ba1e2d9e
Christy Welsh <christyannwelsh@gmail.com>
2018-08-29 12:36:41 -0700

    add delete button

--- client/app.js
+++ client/app.js
@@ -4,21 +4,32 @@ $(document).ready(function () {
     let user;
     let text;
 
-    // handle API request
+    // handle API request (below)
     function handleResponse(data) {
+        // change object into array of objects
         let entries = Object.entries(data)
+        // destructure & extract to chirps array
         for (const [id, chirp] of entries) {
             chirps.push(`${id} ${chirp.user}: ${chirp.text}`);           
         }
+        // remove 'nextid' element in array
         chirps.pop();
-        chirps.map(chirp => $('<p></p>').text(chirp).attr('id', 'chirps').appendTo('.current'))          // create a div containing all the chirps, append to body
+        // map over array, 
+        chirps.map(chirp => {
+            // create a delete button for each chirp, set id
+            let x = $('<button>x</button>').attr('id', 'delete');
+            // create a paragraph containing each chirp, set id, append delete button
+            let p = $(`<p>${chirp}</p>`).attr('id', 'chirps').append(x);
+            // append each paragraph to div
+            $('.current').append(p)
+        })
     }
 
     // use get request to call api
-    $.get('http://127.0.0.1:3000/api/chirps').then(handleResponse).catch(err => console.log(err));  // or use localhost:3000
+    $.get('http://127.0.0.1:3000/api/chirps').then(handleResponse).catch(err => console.log(err));      // or use localhost:3000
 
-    // on button click, get the value of user input and ...
-    $('button').click(() => {
+    // on submit button click, get the value of user inputs and ...
+    $('#submit').click(() => {
         user = $('#user').val();
         text = $('#text').val();
         // make a post request with those values
@@ -28,16 +39,14 @@ $(document).ready(function () {
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify({"user": `${user}`, "text": `${text}`})
         })
-        .then(console.log('posted'))
+        .then()
         .catch(err => console.log(err));
     })
 
-
-
-
-
-
-
+    // on delete button click
+    $('#delete').click( () => {
+        $.delete(`http://127.0.0.1:3000/api/chirps/${?}`)
+    })
 
 
 
