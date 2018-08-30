4e9f3f70159054b81dd1b67714d51771b515f01c
Christy Welsh <christyannwelsh@gmail.com>
2018-08-30 10:43:42 -0700

    add delete function, some bugs

--- client/app.js
+++ client/app.js
@@ -4,22 +4,27 @@ $(document).ready(function () {
     let user;
     let text;
 
-    // handle API request (below)
+    // handle API request (api call below) the server responds with a nested object of chirps
     function handleResponse(data) {
         // change object into array of objects
         let entries = Object.entries(data)
-        // destructure & extract to chirps array
+        // destructure entries array & extract user & text to chirps array
         for (const [id, chirp] of entries) {
-            chirps.push(`${id} ${chirp.user}: ${chirp.text}`);           
+            chirps.push(`${chirp.user}: ${chirp.text}`);
         }
         // remove 'nextid' element in array
         chirps.pop();
         // map over array, 
-        chirps.map(chirp => {
-            // create a delete button for each chirp, set id
-            let x = $('<button>x</button>').attr('id', 'delete');
-            // create a paragraph containing each chirp, set id, append delete button
-            let p = $(`<p>${chirp}</p>`).attr('id', 'chirps').append(x);
+        chirps.map((chirp, index) => {
+            // create a delete button for each chirp, set class
+            let x = $('<button>x</button>').attr('class', 'delete');
+            // create a paragraph containing each chirp
+            let p = $(`<p>${chirp}</p>`).attr({
+                // set an id for styling
+                id: "chirps",
+                // set an index for deleting/updating later
+                index: `${index}`
+            }).append(x);
             // append each paragraph to div
             $('.current').append(p)
         })
@@ -37,19 +42,34 @@ $(document).ready(function () {
             type: "POST",
             url: 'http://127.0.0.1:3000/api/chirps/',
             contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({"user": `${user}`, "text": `${text}`})
+            data: JSON.stringify({ "user": `${user}`, "text": `${text}` })
         })
-        .then()
-        .catch(err => console.log(err));
+            .then()
+            .catch(err => console.log(err));
     })
 
     // on delete button click
-    $('#delete').click( () => {
-        $.delete(`http://127.0.0.1:3000/api/chirps/${?}`)
+    $(document).on("click", ".delete", () => {
+        // set variable for the button's parent (the chirp)
+        let chirpToDelete = $(event.target).parent()
+        // remove html chirp from display
+        chirpToDelete.remove()
+        // also send delete request to remove from server
+        $.ajax({
+            type: "DELETE",
+            url: `http://127.0.0.1:3000/api/chirps/${chirpToDelete.attr('index')}`
+        })
+        .then()
+        .catch(err => console.log(err))
+            
+            
+
+
+
+        // console.log($(event.target).parent().attr('index'))
+        // $(event.target).parent().remove()
+        
     })
-
-
-
 
 
 
